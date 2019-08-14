696582da4aa1307d171f10a9b249bc2fb75df54c
indra <indrahebat@hotmail.com>
2019-08-14 19:28:25 +0900

    added addtrack function in playlist.php, changed permissions to accomodate file write by www-data

--- playlist.php
+++ playlist.php
@@ -8,33 +8,20 @@
 //  path = track's path relative to Music directory home
 //  tr = track number of the track in the playlist
 //
-//plList()
-//outputs list of playlists currently available
 //
-//plGet(pl)
-//outputs json of requested playlists
-//
-//plNewPlaylist(pl)
-//create new empty playlist and put it last
-//
-//plClearPlaylist(pl)
-//
-//plDeletePlaylist(pl)
-//
-//plAddTrack(pl,file_path)
-//add track path and put it last in playlist
-//
-//plRemoveTrack(pl,track_no)
-//
-//plSwapTrack(plname,track_no)
-//swap track track_no and track_no+1, except when its last or first
 
+//filename of playlist data
+
+$pl_fname = ".pldata.json";
+
+//outputs list of playlists currently available
 function plList(){
 	$pljson = file_get_contents(".pldata.json");
 	$plData = json_decode($pljson,true);
 	echo json_encode(array_keys($plData));
 }
 
+//outputs tracklist of requested playlists
 function plGet($plname){
 	$pljson = file_get_contents(".pldata.json");
 	$plData = json_decode($pljson,true);
@@ -42,14 +29,52 @@ function plGet($plname){
 	echo  json_encode($pl);
 }
 
+//push track to a playlist
 function plAddTrack($plname,$file_path){
 	$pljson = file_get_contents(".pldata.json");
 	$plData = json_decode($pljson,true);
 	array_push($plData[$plname],$file_path);
-
-
+	$pljson = json_encode($plData);
+	$status = file_put_contents(".pldata.json",$pljson);
 }
 
+//create new empty playlist and put it last
+function plNewPlaylist($plname){
+	$pljson = file_get_contents(".pldata.json");
+	$plData = json_decode($pljson,true);
+}
+
+function plClearPlaylist($plname){
+	$pljson = file_get_contents(".pldata.json");
+	$plData = json_decode($pljson,true);
+}
+
+function plDelPlaylist($plname){
+	$pljson = file_get_contents(".pldata.json");
+	$plData = json_decode($pljson,true);
+}
+
+function plRemoveTrack($plname,$track_no){
+	$pljson = file_get_contents(".pldata.json");
+	$plData = json_decode($pljson,true);
+}
+
+//swap track track_no and track_no+1, except when its last or first
+function plSwapTrack($plname,$track_no){
+	$pljson = file_get_contents(".pldata.json");
+	$plData = json_decode($pljson,true);
+}
+
+function plInitializeData(){
+	$plData = array("New Playlist 1" => [""]);
+	$pljson = json_encode($plData);
+	var_dump($pljson);
+	$status = file_put_contents($pl_fname,$pljson);
+	var_dump($status);
+	$status = is_writable(".");
+	var_dump($status);
+	echo exec('whoami'); 
+}
 
 //execute according to op parameter
 switch($_GET['op']){
@@ -59,8 +84,26 @@ switch($_GET['op']){
 	case 'get':
 		plGet($_GET['pl']);
 		break;
+	case 'new' :
+		plNewPlaylist($_GET['pl']);
+		break;
+	case 'clr' :
+		plClearPlaylist($_GET['pl']);
+		break;
+	case 'dl' :
+		plDelPlaylist($_GET['pl']);
+		break;
 	case 'add' :
 		plAddTrack($_GET['pl'],$_GET['path']);
+		break;
+	case 'dt' :
+		plRemoveTrack($_GET['pl'],$_GET['tr']);
+		break;
+	case 'sw' :
+		plSwapTrack($_GET['pl'],$_GET['tr']);
+		break;
+	case 'init':
+		plInitializeData();
 		break;
 }
 
